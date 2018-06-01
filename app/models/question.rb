f3a78305d063e649267f8b5656d43216c44f7238
kevin <kevin.lee@mangoplate.com>
2018-06-01 20:50:41 +0900

    더미 컨텐츠를 파라미터로 받기위한 fake method

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ApplicationRecord
   belongs_to :survey
   has_many :answers, :dependent => :destroy
   accepts_nested_attributes_for :answers, reject_if: proc {|a| a['content'].blank?}, allow_destroy: true
+
+  # 더미 컨텐츠를 파라미터로 받기위한 fake method
+  def dummy_content
+  end
 end
